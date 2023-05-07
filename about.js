f6f917b13964a63ad72eac3d56e9a74e1612ae66
Kavin Krishna <95465857+KAVINKRIHNA@users.noreply.github.com>
2023-05-07 13:13:27 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text2", {
+    strings: [ "SSLC: 93.5%", "HSC: 73.0%", "UG: 79.2%"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
